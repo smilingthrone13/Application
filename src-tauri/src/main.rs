08a45e65ded71fc2856426045cbee5f929f218da
Konstantin <lapshin.1998pas@gmail.com>
2023-06-09 14:48:54 +0300

    add autoupdate

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Hello, {}! You've been greeted from version two! Wow this shit sucks!", name)
 }
 
 fn main() {
